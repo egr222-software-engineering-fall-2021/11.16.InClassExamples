2dbe2bb05e76de77a7c6f34bcd046adf9720f9c4
Larry Clement <lclement@calbaptist.edu>
2021-11-15 18:05:52 -0800

    After cleanup

--- Key/ForEachDemo/src/IntegerSet.java
+++ Key/ForEachDemo/src/IntegerSet.java
@@ -1,15 +1,16 @@
 import java.util.*;
 
-public class ForEachLoopDemo {
+public class IntegerSet {
     public static void main(String[] args) {
-//        Set<String> words = new HashSet<String>(); // arbitrary order
+//        Set<Double> grades = new HashSet<>();     // arbitrary order
         Set<Double> grades = new TreeSet<>();   // "natural" order
         grades.add(90.5);
         grades.add(85.4);
         grades.add(100.0);
         grades.add(72.5);
+        grades.add(85.4);   // won't be in the set twice
 
-        System.out.println(grades.size() + " unique words.");
+        System.out.println(grades.size() + " unique scores.");
         for (double grade : grades) {
             System.out.println("Student's grade: " + grade);
         }
